14c829fb0c683d166f0502d61c644c513be1177a
Stefan Falkenstein <sjfalken@gmail.com>
2019-02-14 13:29:08 -0500

    made tab layout and added history tab

--- app/src/main/java/com/example/meditationtimer/MainActivity.kt
+++ app/src/main/java/com/example/meditationtimer/MainActivity.kt
@@ -6,10 +6,17 @@ import android.content.ServiceConnection
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
-import android.util.Log
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Exception
 import java.lang.String.format
 import java.util.*
 
@@ -18,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private var lengthMinutes: Long = 10
     private var timerService : TimerService? = null
     private lateinit var serviceIntent: Intent
+    private lateinit var viewPager: ViewPager
 
     fun setTimerStr(minutes: Long, seconds: Long) {
         findViewById<TextView>(R.id.timer).text =
@@ -96,7 +104,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+        findViewById<TabLayout>(R.id.tabLayout).setupWithViewPager(viewPager)
 
         serviceIntent = Intent(this, TimerService::class.java)
         startService(serviceIntent)
@@ -113,3 +125,47 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+val ARG_WHICH_TAB = "which tab"
+
+class MainPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    // timer and history
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment {
+        val fragment = MainFragment()
+
+        // deciding which layout to use based on the tab position
+        fragment.arguments = Bundle().apply {
+            val arg = when (position) {
+                0 -> R.layout.tab_timer
+                1 -> R.layout.tab_history
+                else -> throw Exception("getItem fragment error")
+            }
+            putInt(ARG_WHICH_TAB, arg)
+        }
+
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        // deciding the tab title based on position
+        return when (position) {
+            0 -> "Timer"
+            1 -> "History"
+            else -> throw Exception("getPageTitle fragment error")
+        }
+    }
+}
+
+class MainFragment() : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // extract the layout arg and inflate
+        return inflater.inflate(arguments!![ARG_WHICH_TAB] as Int,
+            container, false)
+    }
+}
+
