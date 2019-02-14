4e9f8f6edc1ceb2d55daeaf8bdf2995b1fa4ed96
Stefan Falkenstein <sjfalken@gmail.com>
2019-02-14 15:45:26 -0500

    fixed issues with new tab layout

--- app/src/main/java/com/example/meditationtimer/MainActivity.kt
+++ app/src/main/java/com/example/meditationtimer/MainActivity.kt
@@ -22,130 +22,53 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var lengthMinutes: Long = 10
-    private var timerService : TimerService? = null
     private lateinit var serviceIntent: Intent
-    private lateinit var viewPager: ViewPager
-
-    fun setTimerStr(minutes: Long, seconds: Long) {
-        findViewById<TextView>(R.id.timer).text =
-            format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
-    }
-
-
-    fun onStartClick() {
-        setupRunningTimer()
-
-        timerService!!.startTimer(lengthMinutes, 0)
-    }
-
-    fun onStopClick() {
-        setupStoppedTimer()
-
-        timerService!!.stopTimerEarly()
-    }
-
-    fun onPlusClick(view : View) {
-        lengthMinutes += 1
-        setTimerStr(lengthMinutes, 0)
-    }
-
-    fun onMinusClick(view : View) {
-        if (lengthMinutes > 1) lengthMinutes -= 1
-        setTimerStr(lengthMinutes, 0)
-    }
-
-    fun setupRunningTimer() {
-        findViewById<Button>(R.id.startStop).apply {
-            text = "Stop"
-            setOnClickListener { onStopClick() }
-        }
-
-        findViewById<Button>(R.id.plus).visibility = View.GONE
-        findViewById<Button>(R.id.minus).visibility = View.GONE
-    }
-
-    fun setupStoppedTimer() {
-        findViewById<Button>(R.id.startStop).apply {
-            text = "Start"
-            setOnClickListener { onStartClick() }
-        }
-        findViewById<Button>(R.id.plus).visibility = View.VISIBLE
-        findViewById<Button>(R.id.minus).visibility = View.VISIBLE
-        setTimerStr(lengthMinutes, 0)
-
-    }
 
     private val timerConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
-            timerService = (binder as TimerService.TimerBinder).getService()
+            val timerServiceBinder = (binder as TimerService.TimerBinder)
+            val viewPager = findViewById<ViewPager>(R.id.viewPager)
 
-            if (timerService!!.isRunning) {
-                runOnUiThread { setupRunningTimer() }
-            }
-            else {
-                runOnUiThread { setupStoppedTimer() }
-            }
-
-            timerService!!.onTimeChanged = { minutes, seconds ->
-                runOnUiThread { setTimerStr(minutes, seconds) }
-            }
-
-            timerService!!.onTimerFinish = {
-                runOnUiThread { setupStoppedTimer() }
-            }
+            // once the service is connected, setup the tabs
+            viewPager.adapter = MainPagerAdapter(supportFragmentManager, timerServiceBinder)
+            findViewById<TabLayout>(R.id.tabLayout).setupWithViewPager(viewPager)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            timerService = null
         }
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = MainPagerAdapter(supportFragmentManager)
-        findViewById<TabLayout>(R.id.tabLayout).setupWithViewPager(viewPager)
-
         serviceIntent = Intent(this, TimerService::class.java)
-        startService(serviceIntent)
-    }
 
-    override fun onStart() {
-        super.onStart()
+        // this is creating the service if it does not exist
+        startService(serviceIntent)
+
+        // this is creating a connection to the service
         bindService(serviceIntent, timerConnection, 0)
     }
-
-    override fun onStop() {
-        unbindService(timerConnection)
-        super.onStop()
-    }
-
 }
 
-val ARG_WHICH_TAB = "which tab"
+val ARG_SERVICE_BINDER = "service binder"
 
-class MainPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+class MainPagerAdapter(fm : FragmentManager, val timerBinder: TimerService.TimerBinder) :
+    FragmentPagerAdapter(fm) {
+
     // timer and history
     override fun getCount(): Int = 2
 
     override fun getItem(position: Int): Fragment {
-        val fragment = MainFragment()
-
         // deciding which layout to use based on the tab position
-        fragment.arguments = Bundle().apply {
-            val arg = when (position) {
-                0 -> R.layout.tab_timer
-                1 -> R.layout.tab_history
-                else -> throw Exception("getItem fragment error")
+        return when (position) {
+            0 -> TimerFragment().apply {
+                arguments = Bundle().apply { putBinder(ARG_SERVICE_BINDER, timerBinder) }
             }
-            putInt(ARG_WHICH_TAB, arg)
+            1 -> HistoryFragment()
+            else -> throw Exception("getItem fragment error")
         }
-
-        return fragment
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -158,14 +81,97 @@ class MainPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     }
 }
 
-class MainFragment() : Fragment() {
+class TimerFragment() : Fragment() {
+    private lateinit var timerService: TimerService
+    private lateinit var rootView: View
+    private var lengthMinutes: Long = 10
+
+    fun setTimerStr(minutes: Long, seconds: Long) {
+        rootView.findViewById<TextView>(R.id.timer)?.text =
+            format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
+    }
+
+    fun onStartClick() {
+        setupRunningTimer()
+        timerService.startTimer(lengthMinutes, 0)
+    }
+
+    fun onStopClick() {
+        setupStoppedTimer()
+        timerService.stopTimerEarly()
+    }
+
+    fun onPlusClick() {
+        lengthMinutes += 1
+        setTimerStr(lengthMinutes, 0)
+    }
+
+    fun onMinusClick() {
+        if (lengthMinutes > 1) lengthMinutes -= 1
+        setTimerStr(lengthMinutes, 0)
+    }
+
+    fun setupRunningTimer() {
+        rootView.findViewById<Button>(R.id.startStop)?.apply {
+            text = "Stop"
+            setOnClickListener { onStopClick() }
+        }
+
+        rootView.findViewById<Button>(R.id.plus)?.visibility = View.GONE
+        rootView.findViewById<Button>(R.id.minus)?.visibility = View.GONE
+    }
+
+    fun setupStoppedTimer() {
+        rootView.findViewById<Button>(R.id.startStop)?.apply {
+            text = "Start"
+            setOnClickListener { onStartClick() }
+        }
+        rootView.findViewById<Button>(R.id.plus)?.visibility = View.VISIBLE
+        rootView.findViewById<Button>(R.id.minus)?.visibility = View.VISIBLE
+        setTimerStr(lengthMinutes, 0)
+
+    }
+
+
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        // extract the layout arg and inflate
-        return inflater.inflate(arguments!![ARG_WHICH_TAB] as Int,
-            container, false)
+        // get connection to the timer service and configure
+        timerService = (arguments!![ARG_SERVICE_BINDER] as TimerService.TimerBinder).getService()
+
+        rootView = inflater.inflate(R.layout.tab_timer, container, false)
+        // setup the plus and minus buttons
+        rootView.findViewById<Button>(R.id.plus).setOnClickListener { onPlusClick() }
+        rootView.findViewById<Button>(R.id.minus).setOnClickListener { onMinusClick() }
+
+        if (timerService.isRunning) {
+            setupRunningTimer()
+        }
+        else {
+            setupStoppedTimer()
+        }
+
+        timerService.onTimeChanged = { minutes, seconds ->
+            // if the activity is open, update the time on screen
+            activity?.runOnUiThread { setTimerStr(minutes, seconds) }
+        }
+
+        timerService.onTimerFinish = {
+            // if the activity is open, update the ui for stopped timer
+            activity?.runOnUiThread { setupStoppedTimer() }
+        }
+
+        return rootView
+    }
+}
+
+class HistoryFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.tab_history, container, false)
     }
 }
 
