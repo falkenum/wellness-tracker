c6af3827950b9d0486736a766a8c99e6d469837d
Stefan Falkenstein <sjfalken@gmail.com>
2019-02-14 00:13:52 -0500

    made ui changes that aren't working yet

--- app/src/main/java/com/example/meditationtimer/MainActivity.kt
+++ app/src/main/java/com/example/meditationtimer/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.meditationtimer
 
+import android.app.Activity
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.support.constraint.ConstraintLayout
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import java.lang.String.format
 import java.util.*
@@ -17,13 +21,36 @@ class MainActivity : AppCompatActivity() {
     private var timerService : TimerService? = null
     private lateinit var serviceIntent: Intent
 
-    fun onStartClick(view : View) {
+    // making a parent view where I can easily modify the three buttons I need
+    class TimerLayout(context : Activity) : ConstraintLayout(context) {
+        lateinit var plus : Button
+        lateinit var minus : Button
+        lateinit var startStop : Button
+        lateinit var timer : TextView
+        init {
+            context.layoutInflater.inflate(R.layout.activity_main, this)
+            for (i in 0..childCount) when (getChildAt(i).id) {
+                R.id.plus -> plus = getChildAt(i) as Button
+                R.id.minus -> minus = getChildAt(i) as Button
+                R.id.startStop -> startStop = getChildAt(i) as Button
+                R.id.timer -> timer = getChildAt(i) as TextView
+            }
+        }
+
+        fun setTimerStr(minutes: Long, seconds: Long) {
+            timer.text = format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
+        }
+    }
+    private lateinit var parent: TimerLayout
+
+
+    fun onStartClick() {
         setupRunningTimer()
 
         timerService!!.startTimer(lengthMinutes, 0)
     }
 
-    fun onStopClick(view : View) {
+    fun onStopClick() {
         setupStoppedTimer()
 
         timerService!!.stopTimerEarly()
@@ -31,21 +58,33 @@ class MainActivity : AppCompatActivity() {
 
     fun onPlusClick(view : View) {
         lengthMinutes += 1
-        setTimerStr(lengthMinutes, 0)
+        parent.setTimerStr(lengthMinutes, 0)
     }
 
     fun onMinusClick(view : View) {
         if (lengthMinutes > 1) lengthMinutes -= 1
-        setTimerStr(lengthMinutes, 0)
+        parent.setTimerStr(lengthMinutes, 0)
     }
 
     fun setupRunningTimer() {
-        setContentView(R.layout.timer_running)
+        parent.startStop.apply {
+            text = "Stop"
+            setOnClickListener { onStopClick() }
+        }
+        parent.setTimerStr(lengthMinutes, 0)
+        parent.plus.visibility = View.GONE
+        parent.minus.visibility = View.GONE
     }
 
     fun setupStoppedTimer() {
-        setContentView(R.layout.timer_stopped)
-        setTimerStr(lengthMinutes, 0)
+        parent.startStop.apply {
+            text = "Start"
+            setOnClickListener { onStartClick() }
+        }
+        parent.setTimerStr(lengthMinutes, 0)
+        parent.plus.visibility = View.VISIBLE
+        parent.minus.visibility = View.VISIBLE
+
     }
 
     private val timerConnection = object : ServiceConnection {
@@ -60,7 +99,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             timerService!!.onTimeChanged = { minutes, seconds ->
-                runOnUiThread { setTimerStr(minutes, seconds) }
+                runOnUiThread { parent.setTimerStr(minutes, seconds) }
             }
 
             timerService!!.onTimerFinish = {
@@ -73,13 +112,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setTimerStr(minutes: Long, seconds: Long) {
-        findViewById<TextView>(R.id.timerView).text =
-            format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        parent = TimerLayout(this)
+        setContentView(parent)
 
         serviceIntent = Intent(this, TimerService::class.java)
         startService(serviceIntent)
